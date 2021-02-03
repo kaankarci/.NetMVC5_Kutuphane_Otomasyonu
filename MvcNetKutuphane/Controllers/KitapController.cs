367cac73f1b4e2e4da05539d5b9b2f68f5eaf7bd
Ömer Kaan KARCI <kaankarci@gmail.com>
2021-02-03 18:33:58 +0300

    Kitap ekleme ve Silme eklendi!

--- MvcNetKutuphane/Controllers/KitapController.cs
+++ MvcNetKutuphane/Controllers/KitapController.cs
@@ -17,5 +17,45 @@ namespace MvcNetKutuphane.Controllers
 
             return View(kitaplar);
         }
+        [HttpGet]
+        public ActionResult KitapEkle() {
+            List<SelectListItem> ktgrcek = (from i in db.TBLKATEGORI.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = i.ADI,
+                                               Value = i.ID.ToString()
+                                           }).ToList();
+
+            List<SelectListItem> yzrcek = (from i in db.TBLYAZAR.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = i.AD + " " + i.SOYAD,
+                                               Value = i.ID.ToString()
+                                           }).ToList();
+
+
+            ViewBag.yzrlist = yzrcek;
+            ViewBag.ktgrlist = ktgrcek;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult KitapEkle(TBLKITAP p) {
+
+            var ktg = db.TBLKATEGORI.Where(k => k.ID == p.TBLKATEGORI.ID).FirstOrDefault();
+            var yzr = db.TBLYAZAR.Where(k => k.ID == p.TBLYAZAR.ID).FirstOrDefault();
+            p.TBLKATEGORI = ktg;
+            p.TBLYAZAR = yzr;
+            db.TBLKITAP.Add(p);
+            db.SaveChanges();
+            
+
+            return RedirectToAction("Index");
+        }
+        public ActionResult KitapSil(int id) {
+            var kitap = db.TBLKITAP.Find(id);
+            db.TBLKITAP.Remove(kitap);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
